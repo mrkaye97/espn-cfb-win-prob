1c24b23110af5b8910fee12837c8d5170996fcbe
mrkaye97 <mrkaye97@gmail.com>
2023-01-01 13:11:18 -0500

    more cleanup

--- winprob/load-pbp.R
+++ winprob/load-pbp.R
@@ -2,28 +2,44 @@ library(DBI)
 library(RPostgres)
 library(cfbfastR)
 library(dplyr)
+library(DBI)
+library(bit64)
 
 source("helpers.R")
 source("constants.R")
 
-conn <- dbConnect(
-  Postgres(),
-  dbname = "cfb"
-)
+conn <- connect_to_db()
 
-pbp <- load_cfb_pbp(years)
+raw_pbp <- load_cfb_pbp(years)
 pbp_2022 <- load_cfb_pbp(2022)
 
-pbp <- select(
-  pbp,
-  all_of(
-    colnames(pbp_2022)
+pbp <- raw_pbp %>%
+  select(
+    all_of(
+      colnames(pbp_2022)
+    )
+  ) %>%
+  mutate(
+    play_id = as.character(as.integer64(id_play)),
+    .before = id_play
+  ) %>%
+  select(
+    -id_play
   )
-)
 
 dbCopy(
   conn,
   "pbp",
   pbp,
-  truncate = TRUE
+  drop = TRUE
+)
+
+dbExecute(
+  conn,
+  "
+  ALTER TABLE pbp
+  ALTER COLUMN play_id
+  TYPE BIGINT
+  USING play_id::BIGINT
+  "
 )
