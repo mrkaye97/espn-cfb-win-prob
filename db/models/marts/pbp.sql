21efb1f0f779517636fba36872e9139299cc0fbb
mrkaye97 <mrkaye97@gmail.com>
2023-01-02 13:17:46 -0500

    sql cleanup

--- db/models/marts/pbp.sql
+++ db/models/marts/pbp.sql
@@ -10,15 +10,6 @@ WITH pbp AS (
     SELECT *
     FROM {{ source("raw", "pbp") }}
 ),
-clocks AS (
-    SELECT 
-        game_id, 
-        play_id, 
-        "clock.minutes" AS minutes, 
-        "clock.seconds" AS seconds, 
-        period
-    FROM pbp
-),
 last_plays AS (
     SELECT game_id, MAX(play_id) AS play_id
     FROM pbp
@@ -36,13 +27,14 @@ game_winners as (
 ),
 home_away_scores AS (
     SELECT
-      play_id,
-      CASE WHEN offense_play = home THEN offense_score ELSE defense_score END AS home_score,
-      CASE WHEN offense_play = away THEN offense_score ELSE defense_score END AS away_score
+        game_id,
+        play_id,
+        CASE WHEN offense_play = home THEN offense_score ELSE defense_score END AS home_score,
+        CASE WHEN offense_play = away THEN offense_score ELSE defense_score END AS away_score
     FROM pbp
 ),
 win_probs AS (
-    SELECT DISTINCT play_id::BIGINT, home_win_prob
+    SELECT DISTINCT game_id::INT, play_id::INT, home_win_prob
     FROM {{ source("raw", "espn_win_probs") }}
 )
 
@@ -51,16 +43,14 @@ SELECT DISTINCT
     p.play_id,
     p.home AS teams__home,
     p.away AS teams__away,
-    c.minutes AS clock__minutes_remaining,
-    c.seconds AS clock__seconds_remaining,
-    c.period AS clock__period,
+    p.clock_minutes AS clock__minutes_remaining,
+    p.clock_seconds AS clock__seconds_remaining,
+    p.period AS clock__period,
     s.home_score AS scores__home,
     s.away_score AS scores__away,
     wp.home_win_prob,
     gw.home_win
 FROM pbp p
-LEFT JOIN clocks c USING(play_id)
-LEFT JOIN home_away_scores s USING(play_id)
+LEFT JOIN home_away_scores s USING(game_id, play_id)
+LEFT JOIN win_probs wp USING(game_id, play_id)
 LEFT JOIN game_winners gw ON p.game_id = gw.game_id
-LEFT JOIN win_probs wp ON wp.play_id::BIGINT = p.play_id::BIGINT
-ORDER BY game_id, play_id, clock__period, clock__minutes_remaining DESC, clock__seconds_remaining DESC
