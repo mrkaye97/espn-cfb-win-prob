2ba0d558f0e4d9e2568629ca9b9f51b92e3ca945
mrkaye97 <mrkaye97@gmail.com>
2023-01-02 16:40:39 -0500

    rework how we load the pbp data

--- winprob/load-pbp.R
+++ winprob/load-pbp.R
@@ -5,34 +5,44 @@ library(dplyr)
 library(DBI)
 library(bit64)
 library(secret)
+library(purrr)
+library(furrr)
+library(lubridate)
 
 source("helpers.R")
 source("constants.R")
 
-raw_pbp <- load_cfb_pbp(years)
-pbp_2022 <- load_cfb_pbp(2022)
+Sys.setenv(CFBD_API_KEY = get_secret("CFBD_API_KEY"))
 
-pbp <- raw_pbp %>%
-  select(
-    all_of(
-      colnames(pbp_2022)
-    )
-  ) %>%
-  mutate(
-    game_id = as.character(as.integer64(game_id)),
-    play_id = stringr::str_remove(
-      as.character(as.integer64(id_play)),
-      game_id
-    ),
-    .before = id_play
-  ) %>%
-  select(
-    -id_play
-  ) %>%
-  mutate(
-    game_id = as.integer(game_id),
-    play_id = as.integer(play_id)
-  )
+game_ids <- map(
+  years,
+  ~ cfbd_game_info(year = .x)$game_id
+) %>%
+  flatten_int()
+
+plan(multisession, workers = 10)
+
+pbp <- future_map_dfr(
+  game_ids,
+  ~ {
+    espn_cfb_pbp(.x) %>%
+      mutate(game_id = .x) %>%
+      transmute(
+        season,
+        week,
+        game_id,
+        play_id = as.integer(stringr::str_sub(plays_id, nchar(.x) + 1, nchar(plays_id))),
+        home_team,
+        away_team,
+        home_score = plays_home_score,
+        away_score = plays_away_score,
+        wall_clock_time = plays_wallclock,
+        clock_time_minutes = ms(plays_clock_display_value) %>% minute(),
+        clock_time_seconds = ms(plays_clock_display_value) %>% second(),
+        clock_period = plays_period_number
+      )
+  }
+)
 
 dbCopy(
   db_url = get_secret("DB_URL"),
