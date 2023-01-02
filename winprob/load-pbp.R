b892f9e0afab2fb90ad0241abee7cab02056cb56
mrkaye97 <mrkaye97@gmail.com>
2023-01-02 13:16:35 -0500

    more cleanup

--- winprob/load-pbp.R
+++ winprob/load-pbp.R
@@ -19,11 +19,34 @@ pbp <- raw_pbp %>%
     )
   ) %>%
   mutate(
-    play_id = as.character(as.integer64(id_play)),
+    game_id = as.character(as.integer64(game_id)),
+    play_id = stringr::str_remove(
+      as.character(as.integer64(id_play)),
+      game_id
+    ),
     .before = id_play
   ) %>%
   select(
     -id_play
+  ) %>%
+  mutate(
+    game_id = as.integer(game_id),
+    play_id = as.integer(play_id)
+  ) %>%
+  select(
+    year,
+    week,
+    play_id,
+    game_id,
+    offense_play,
+    defense_play,
+    offense_score,
+    defense_score,
+    home,
+    away,
+    clock_minutes = clock.minutes,
+    clock_seconds = clock.seconds,
+    period
   )
 
 dbCopy(
@@ -32,4 +55,15 @@ dbCopy(
   table = "pbp",
   data = pbp,
   drop = TRUE
+)
+
+conn <- connect_to_db()
+
+dbExecute(
+  conn,
+  "
+  CREATE INDEX index_raw_pbp_on_game_id_play_id
+  ON raw.pbp
+  (game_id, play_id)
+  "
 )
--- winprob/load-win-probs.R
+++ winprob/load-win-probs.R
@@ -5,6 +5,7 @@ library(furrr)
 library(dplyr)
 library(bit64)
 library(secret)
+library(DBI)
 
 source("helpers.R")
 
@@ -12,7 +13,7 @@ conn <- connect_to_db()
 
 games <- query(
   conn,
-  "select distinct game_id from pbp"
+  "select distinct game_id from raw.pbp"
 )$game_id
 
 safely_get_wp <- safely(get_espn_wp_college)
@@ -34,8 +35,11 @@ wps <- future_map_dfr(
     result %>%
       tibble() %>%
       transmute(
-        game_id = as.integer(espn_game_id),
-        play_id = as.character(as.integer64(play_id)),
+        game_id = as.character(as.integer64(espn_game_id)),
+        play_id = stringr::str_remove(
+          as.character(as.integer64(play_id)),
+          game_id
+        ),
         home_win_prob = home_win_percentage
       )
   }
@@ -47,9 +51,18 @@ dbCopy(
   table = "espn_win_probs",
   data = wps,
   fields = list(
-    game_id = "BIGINT",
-    play_id = "BIGINT",
+    game_id = "INT",
+    play_id = "INT",
     home_win_prob = "FLOAT"
   ),
   drop = TRUE
+)
+
+dbExecute(
+  conn,
+  "
+  CREATE INDEX index_raw_wps_on_game_id_play_id
+  ON raw.espn_win_probs
+  (game_id, play_id)
+  "
 )
