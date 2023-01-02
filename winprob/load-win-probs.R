b0d06d5ee43bdfb864f41c752e6c2e8ba557ea2d
mrkaye97 <mrkaye97@gmail.com>
2023-01-02 16:03:20 -0500

    bulletproofing wp getting a bit

--- winprob/load-win-probs.R
+++ winprob/load-win-probs.R
@@ -16,33 +16,40 @@ games <- query(
   "select distinct game_id from raw.pbp"
 )$game_id
 
-safely_get_wp <- safely(get_espn_wp_college)
-
 plan(multisession, workers = 10)
 
-wps <- future_map_dfr(
+wps <- future_map(
   games,
   ~ {
-    result <- safely_get_wp(.x) |>
-      pluck("result")
+    result <- get_espn_wp_college(.x)
 
-    if (nrow(result) == 0) {
-      return(
-        tibble()
-      )
+    if (nrow(result$result) > 0) {
+      result$result <- result$result %>%
+        transmute(
+          game_id = as.character(as.integer64(espn_game_id)),
+          play_id = stringr::str_remove(
+            as.character(as.integer64(play_id)),
+            game_id
+          ),
+          home_win_prob = home_win_percentage
+        )
     }
 
-    result %>%
-      tibble() %>%
-      transmute(
-        game_id = as.character(as.integer64(espn_game_id)),
-        play_id = stringr::str_remove(
-          as.character(as.integer64(play_id)),
-          game_id
-        ),
-        home_win_prob = home_win_percentage
-      )
+    return(result)
   }
+)
+
+successes <- wps %>%
+  keep(
+    ~ nrow(.x$result) > 0
+  ) %>%
+  map_dfr(
+    ~ .x$result
+)
+
+failures <- keep(
+    wps,
+    ~ nrow(.x$result) == 0
 )
 
 dbCopy(
