7ff32e247edffda53e17898c5213140a4ac3a463
mrkaye97 <mrkaye97@gmail.com>
2023-01-14 16:53:51 -0500

    script to load the data

---
+++ winprob/R/load-clean-data.R
@@ -0,0 +1,27 @@
+source("R/helpers.R")
+source("R/data-wrangling.R")
+source("R/diagnostics.R")
+source("R/plotting.R")
+
+conn <- connect_to_db()
+
+raw <- query(
+  conn,
+  "
+  SELECT *
+  FROM cfb.espn_diagnostics
+  WHERE home_win_prob IS NOT NULL
+  "
+)
+
+clean <- raw |>
+  remove_negative_score_games() |>
+  generate_timestamps() |>
+  enforce_rough_monotonicity() |>
+  dplyr::mutate(
+    home_win_fct = as.factor(home_win)
+  )
+
+DBI::dbDisconnect(conn)
+
+rm(list = ls()[ls() != "clean"])
